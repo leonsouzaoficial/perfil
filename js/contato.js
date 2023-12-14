e207566f7749d8b8eef80e87a7bac178db0b51bd
leonsouzaoficial <leonsouzasilvaoficial@gmail.com>
2023-12-14 19:29:16 -0300

    site pronto

--- js/contato.js
+++ js/contato.js
@@ -1,18 +1,31 @@
-//let div_currículo = document.getElementById("currículo")
-//let img_foto_do_perfil = document.getElementById("foto_de_perfil")
-let div_título = document.getElementById("título")
+let div_currículo = document.getElementById("currículo")
+let img_foto_do_perfil = document.getElementById("foto_de_perfil_currículo")
+let div_título = document.querySelector("div#título")
 
 
 // posiciona o título
 function posicionarTítulo () {
-    div_título.style.marginTop = -150 + "px"
+    div_título.style.marginTop = "-150px"
+    div_título.style.marginLeft = "165px"
+    div_título.style.width = (div_currículo.clientWidth - img_foto_do_perfil.clientWidth - 40) + "px"
+}
+
+// posiciona o botão
+function posicionarBotão () {
+    let bt = document.getElementById("bt_imprimir")
+
+    bt.style.marginLeft = (div_currículo.clientWidth-bt.clientWidth)/2 + "px"
+}
+
+function imprimir () {
+    print()
 }
 
 function principal () {
     posicionarTítulo()
+    posicionarBotão()
 
     requestAnimationFrame(principal)
 }
 
 principal()
-alert("ok")
